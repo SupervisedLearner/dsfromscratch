1c46fddaed39b5cacfd6985c0af7ba215d45c6e4
Kenneth Lee <kenneth.lee@ironshore.com>
2018-06-15 14:29:35 -0400

    added basic statistics and probability functions

--- matrix.py
+++ matrix.py
@@ -112,3 +112,113 @@ def identity(n):
     #returns an identity matrix of size nxn
     return make_matrix(n,n,is_diagonal)
 
+##Summary statistics functions
+def mean(x):
+    return sum(x)/len(x)
+
+def median(x):
+    n = len(x)
+    sorted_x = sorted(x)
+    midpoint = n // 2
+    
+    if n % 2 == 1:
+        #if the length is odd, just return the middle value
+        return sorted_x[midpoint]
+    else:
+        #if the length is even, return the average of the two middle values
+        lo = midpoint -1
+        hi = midpoint
+        return ((sorted_x[lo]+sorted_x[hi]) / 2)
+    
+def quantile(x,p):
+    #returns the p-th percentile value in x
+    p_index = int(p*len(x))
+    return sorted(x)[p_index]
+
+import collections
+def mode(x):
+    #returns a list, might be more than one mode
+    counts = collections.Counter(x)
+    max_count = max(counts.values())
+    return [x_i for x_i, count in counts.items()
+            if count == max_count]
+
+def data_range(x):
+    return max(x) - min(x)
+
+
+def de_mean(x):
+    #returns the deviations from the mean (x-E(x))
+    x_bar = mean(x)
+    return [x_i - x_bar for x_i in x]
+
+def variance(x):
+    #this is sample variance
+    n = len(x)
+    deviations = de_mean(x)
+    return sum_of_squares(deviations)/(n-1)
+
+def standard_deviation(x):
+    return math.sqrt(variance(x))    
+    
+def interquartile_range(x):
+    return quantile(x,0.75) - quantile(x, 0.25)
+
+def covariance(x,y):
+    n = len(x)
+    return dot(de_mean(x), de_mean(y)) / (n-1)
+
+
+def correlation(x,y):
+    stdev_x = standard_deviation(x)
+    stdev_y = standard_deviation(y)
+    if stdev_x > 0 and stdev_y > 0:
+        return covariance(x,y)/(stdev_x*stdev_y)
+    else:
+        return 0 #technically it's undefined, but we return 0
+    
+def uniform_pdf(x):
+    return 1 if x >= 0 and x < 1 else 0
+
+def uniform_cdf(x):
+    if x < 0 : return 0
+    elif x < 1 : return x
+    else: return 1
+    
+def normal_pdf(x, mu = 0, sigma = 1):
+    sqrt_two_pi = math.sqrt(2*math.pi)
+    return (math.exp(-(x-mu) ** 2 / 2  / sigma ** 2)/(sqrt_two_pi*sigma))
+
+def normal_cdf(x, mu = 0 , sigma = 1):
+    return (1+math.erf((x-mu)/math.sqrt(2)/sigma)) /2
+
+def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
+    #find approximate inverse using binary search
+    if mu != 0 or sigma !=1:
+        return mu + sigma * inverse_normal_cdf(p, tolerance = tolerance)
+    low_z, low_p = -10.0,0
+    hi_z, hi_p = 10.0,1
+    while hi_z - low_z > tolerance:
+        mid_z = (low_z+hi_z) / 2
+        mid_p = normal_cdf(mid_z)
+        if mid_p < p:
+            low_z, low_p = mid_z, mid_p
+        elif mid_p > p:
+            hi_z, hi_p = mid_z, mid_p
+        else: 
+            break
+    return mid_z
+
+import random
+def bernoulli_trial(p):
+    return 1 if random.random() < p else 0
+
+def binomial(n, p):
+    return sum(bernoulli_trial(p) for _ in range(n))
+
+def make_hist(p, n, num_points):
+    #use a bar chart to show actual binomial samples
+    return None
+    
+
+        
